f346b2d3dbcea094ee8c9d6ca5e920c30f2b1758
wahabchota76-maker <wahabchota76@gmail.com>
2026-08-12 01:33:10 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="dq-final-1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./app.js","./manifest.json","./icon.svg"]))));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
